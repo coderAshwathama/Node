d703d5afe8350db80d872ddace4ba508fd4d6808
Rijan Tiwari <reezantiwaree@gmail.com>
2026-06-18 09:08:51 +0545

    testing data

--- day-1/express-essentials/index.js
+++ day-1/express-essentials/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-
+import data from "./data/mock.json" with { type: "json" };
 const app = express();
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log("The server is running on port  ", PORT);
+  console.log(data);
 });
