66f1c62c79bd238d35709ea1571f2720fdcaadd0
Rijan Tiwari <reezantiwaree@gmail.com>
2026-06-18 09:11:19 +0545

    first route

--- day-1/express-essentials/index.js
+++ day-1/express-essentials/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3001;
 
+app.get("/", (request, response) => {
+  response.send("This is a GET request at /");
+});
+
 app.listen(PORT, () => {
   console.log("The server is running on port  ", PORT);
   console.log(data);
