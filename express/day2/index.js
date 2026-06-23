e38e5e49f13c9c24a8d82bea35de66f388b14a16
Rijan Tiwari <reezantiwaree@gmail.com>
2026-06-23 09:18:25 +0545

    added express routers

--- express/day2/index.js
+++ express/day2/index.js
@@ -1,11 +1,6 @@
 import express from "express";
 const app = express();
 
-function sayHiMiddleware(req, res, next) {
-  console.log("HI I'm a Middleware");
-  next();
-}
-
 // app.use(sayHiMiddleware);
 app.get("/", sayHiMiddleware, (req, res) => {
   res.send("Hello World");
