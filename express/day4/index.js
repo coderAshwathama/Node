083756781c1d4faa1ed50791cb0ece6eca73a20e
Rijan Tiwari <reezantiwaree@gmail.com>
2026-06-24 11:55:20 +0545

    added a new packages cookie parser and express session

--- express/day4/index.js
+++ express/day4/index.js
@@ -4,8 +4,36 @@ import session from "express-session";
 const app = express();
 const PORT = 8080;
 
+app.use(
+  session({
+    secret: "mysecret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, //1 day
+    },
+  }),
+);
+
 app.get("/", (req, res) => {
-  res.status(200).res.send("Hello World");
+  console.log(req.session);
+  console.log(req.session.id);
+  res.status(200).send("Hello World");
+});
+
+app.get("/login", (req, res) => {
+  req.session.user = {
+    name: "john",
+    email: "john@hello.com",
+    age: 30,
+  };
+
+  res.send(`${req.session.user.name} is Logged In `);
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.send("User Logged Out");
 });
 
 app.listen(PORT, () => {
